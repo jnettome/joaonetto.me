e857c6ba6f92a84af2b1e87335cacf76a2d61d6a
João Netto <hi@joaonetto.me>
2012-08-01 12:44:32 -0300

    Capistrano configurado, deploy funcionando

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,25 @@
+set :user, "joaonettome"
+set :domain, "mytrix"
 default_run_options[:pty] = true
 
-set :application, "joaonetto.me"
-
 set :repository,  "git@bitbucket.org:joaonettome/joaonetto.me.git"
+set :deploy_to, "/var/www/joaonetto.me"
+set :deploy_via, :remote_cache
 set :scm, :git
 set :scm_passphrase, "Phdxp103!@#"
 set :scm_verbose, true
-set :deploy_via, :remote_cache
-set :deploy_to, "/var/www/joaonetto.me"
 set :use_sudo, true
 
-set :user, "joaonettome"
-set :domain, "mytrix"
-
-role :web, "apache2"                          # Your HTTP server, Apache/etc
 server domain, :app, :web
 
 namespace :deploy do
+  task :bundle do
+    run "cd #{current_path} && bundle install"
+  end
+  
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+
+after "deploy", "deploy:bundle"
